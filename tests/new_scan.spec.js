6399c31062bff7e497b54f1ce8670538b9e9fe6f
rachini <rachini94perera@gmail.com>
2024-04-09 08:49:47 +0530

    test2

--- tests/new_scan.spec.js
+++ tests/new_scan.spec.js
@@ -1,9 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 
-test('test', async ({ page }) => {
+test('scan new contact', async ({ page }) => {
     test.setTimeout(120000);
-
 
     //login to IFS cloud
     await page.goto('https://topo-d02.build.ifsdemoworld.com/');
@@ -18,16 +17,24 @@ test('test', async ({ page }) => {
 
 
     //verify the text
-    await expect(page.locator('//*[@id="ScanNewContactAssistant-StepIndicatorLabel-0"]')).toHaveText('Scan Business Card',{ timeout: 50000 });
+    await expect(page.locator('//*[@id="ScanNewContactAssistant-StepIndicatorLabel-0"]')).toHaveText('Scan Business Card', { timeout: 50000 });
 
     //image upload
     const fileChooserPromise = page.waitForEvent('filechooser');
     await page.getByText('Browse').click();
     const fileChooser = await fileChooserPromise;
     await fileChooser.setFiles('/Users/admin/Downloads/BC002.png');
+
+    //verify the save button name and its enable
+    await expect(page.locator('div').filter({ hasText: /^Save$/ })).toHaveText('Save', { timeout: 50000 });;
+    await expect(page.locator('div').filter({ hasText: /^Save$/ })).toBeEnabled();
     await page.getByText('Save').click();
+
+    //verify the delete button and its enable
+    await expect(page.locator('div').filter({ hasText: /^Delete$/ })).toHaveText('Delete', { timeout: 50000 });
+    await expect(page.locator('div').filter({ hasText: /^Delete$/ })).toBeEnabled();
     await page.getByRole('button', { name: 'Next' }).click();
-    
+
 
     //verify the toast message after click Next
     await expect(page.locator('role=alert')).toHaveText('Scanning uploaded image', { timeout: 50000 });
@@ -36,35 +43,34 @@ test('test', async ({ page }) => {
     await expect(page.locator('id=ScanNewContactAssistant-StepIndicatorLabel-1')).toHaveText('Review Scanned Values');
 
     //select values
-    await page.locator('#ScanNewContactAssistant-ScanImageResultHeaderGroup-Company').getByRole('button', { name: '' }).click();
+    await page.locator('#ScanNewContactAssistant-ScanImageResultHeaderGroup-Company').getByRole('button', { class: 'picker-button ng-tns-c212-1572 dropdown-mode ng-star-inserted' }).click();
     await page.locator('fnd-lov-entry').getByText('VENTECHI INC.').click();
 
-    await page.locator('#ScanNewContactAssistant-ScanImageResultHeaderGroup-Name').getByRole('button', { name: '' }).click();
+    await page.locator('#ScanNewContactAssistant-ScanImageResultHeaderGroup-Name').getByRole('button', { class: 'picker-button ng-tns-c212-1572 dropdown-mode ng-star-inserted' }).click();
     await page.locator('fnd-lov-entry').getByText('Mike Miller').click();
 
-    await page.locator('#ScanNewContactAssistant-ScanImageResultHeaderGroup-JobTitle').getByRole('button', { name: '' }).click();
+    await page.locator('#ScanNewContactAssistant-ScanImageResultHeaderGroup-JobTitle').getByRole('button', { class: 'picker-button ng-tns-c212-1572 dropdown-mode ng-star-inserted' }).click();
     await page.locator('fnd-lov-entry').getByText('CEO').click();
 
-    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-AddressLine').getByRole('button', { name: '' }).click();
+    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-AddressLine').getByRole('button', { class: 'picker-button ng-tns-c212-1572 dropdown-mode ng-star-inserted' }).click();
     await page.locator('fnd-lov-entry').getByText('Massachusetts Avenue Northwest').click();
 
-    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-State').getByRole('button', { name: '' }).click();
+    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-State').getByRole('button', { class: 'picker-button ng-tns-c212-1572 dropdown-mode ng-star-inserted' }).click();
     await page.locator('fnd-lov-entry').getByText('DC').click();
 
-    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-County').getByRole('button', { name: '' }).click();
-    await page.locator('fnd-lov-entry div').first().click();
-
-    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-County').getByRole('button', { name: '' }).click();
+    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-County').getByRole('button', { class: 'picker-button ng-tns-c212-1572 dropdown-mode ng-star-inserted' }).click();
     await page.locator('fnd-lov-entry').getByText('District of Columbia').click();
 
+    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-City').getByRole('button', { class: 'picker-button ng-tns-c212-1572 dropdown-mode ng-star-inserted' }).click();
+    await page.locator('fnd-lov-entry').getByText('Washington').click();
 
-    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-ZipCode').getByRole('button', { name: '' }).click();
+    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-ZipCode').getByRole('button', { class: 'picker-button ng-tns-c212-1572 dropdown-mode ng-star-inserted' }).click();
     await page.locator('fnd-lov-entry').getByText('20036').click();
 
-    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-Country').getByRole('button', { name: '' }).click();
+    await page.locator('#ScanNewContactAssistant-ScanImageResultAddressGroup-Country').getByRole('button', { class: 'picker-button ng-tns-c212-1572 dropdown-mode ng-star-inserted' }).click();
     await page.locator('fnd-lov-entry').getByText('United States').click();
 
-    await page.locator('#ScanNewContactAssistant-ScanImageResultContactGroup-Phone').getByRole('button', { name: '' }).click();
+    await page.locator('#ScanNewContactAssistant-ScanImageResultContactGroup-Phone').getByRole('button', { class: 'picker-button ng-tns-c212-1572 dropdown-mode ng-star-inserted' }).click();
     await page.locator('fnd-lov-entry').getByText('+1-202-782-').click();
 
     await page.locator('#ScanNewContactAssistant-ScanImageResultContactGroup-Email').getByRole('button', { name: '' }).click();
@@ -72,6 +78,8 @@ test('test', async ({ page }) => {
 
     //click 'Launch New Contact Assistant' button
     await page.getByRole('button', { name: 'Launch New Contact Assistant' }).click();
+
+    //verify page name 'New Contact'
     await expect(page.locator('//*[@id="AddContactAssistant"]/div/div/div[1]/div[1]/h1')).toHaveText('New Contact')
 
     //Turn off New
